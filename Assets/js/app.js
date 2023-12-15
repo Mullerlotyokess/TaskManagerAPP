4a1b3d17c673f5611a0ea646e15c28b2cd3869da
Mullerlotyokess <bucifej09@gmail.com>
2023-12-15 22:25:46 +0100

    xdasd

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -6,11 +6,11 @@ app.run(($rootScope) => {
 
     $rootScope.emailRegExp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     $rootScope.passwdRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
-    
+
     $rootScope.serverUrl = 'http://localhost:5000';
     $rootScope.appUrl = 'http://127.0.0.1:5500/index.html';
-    
-    if(sessionStorage.getItem('access_token')){
+
+    if (sessionStorage.getItem('access_token')) {
         $rootScope.loggedIn = true;
         token = JSON.parse(sessionStorage.getItem('access_token'));
         axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
@@ -40,7 +40,7 @@ app.config(($routeProvider) => {
             controller: 'statCtrl',
         })
         .otherwise({
-            redirectTo: '/login'
+            redirectTo: 'Views/main.html'
         });
-    }
+}
 );
